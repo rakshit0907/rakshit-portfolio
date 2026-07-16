02ea232ecc1c5a63beb6ee4f8baf3489cb1a18a4
Rakshit Pandey <rakshitpandey0321@gmail.com>
2026-07-17 02:13:37 +0530

    chore: prepare production build

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          three: [
-            "three",
-            "@react-three/fiber",
-            "@react-three/drei",
-          ],
-
-          gsap: ["gsap"],
-
-          react: [
-            "react",
-            "react-dom",
-            "react-router-dom",
-          ],
-        },
-      },
-    },
-  },
+  plugins: [react()],
 });
